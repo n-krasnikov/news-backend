3869de92aa6ef81a551535fa5ef5831751ae71ce
Nitita Krasnikov <n.krasnikov@tg.dunice.net>
2023-06-16 10:55:47 +0300

    ViewSet to ModelViewSet

--- news_api/views/posts.py
+++ news_api/views/posts.py
@@ -1,15 +1,11 @@
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
 from ..models import Post, User
 from ..serializers import PostSerializer
 
-class PostsViewSet(ViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    def list(self, _):
-        post = Post.objects.all()
-        serializer = PostSerializer(post, many=True)
-        return Response(serializer.data)
+class PostsViewSet(ModelViewSet):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
