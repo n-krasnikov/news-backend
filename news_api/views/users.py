1e12a3dc41e2fd22d0c2a09eec8586f4e32b286a
Nitita Krasnikov <n.krasnikov@tg.dunice.net>
2023-07-12 10:58:13 +0300

    add IsAuth perm UserView

--- news_api/views/users.py
+++ news_api/views/users.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from ..models import User
 from ..serializers import UserSerializer
 
 class UsersViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
